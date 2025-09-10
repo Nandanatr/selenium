c83f3fb7012f2daf4262561bcf6e579dc5703647
DELL <DELL@192.168.1.40>
2025-09-10 12:05:51 +0530

    commited

---
+++ Kochi/src/test/java/Demo/sample.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hai");
+		System.out.println("hai");
+
+		System.out.println("hai");
+
+
+	}
+
+}
